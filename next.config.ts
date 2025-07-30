0fe839807ae22f69c3e4664960df3347c0fb0d87
Logesh M <mlogesh2996@gmail.com>
2025-07-30 09:09:26 +0530

    static build

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  trailingSlash: true, // required for GitHub Pages routing
+  basePath: '/todo-task', // IMPORTANT: match your repo name
   reactStrictMode: true,
-  experimental: {
-    useLightningcss: false,
-  },
 };
 
 export default nextConfig;
